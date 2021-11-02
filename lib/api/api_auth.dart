8958c07bb7df5b34e8b2022ca04f34663ad73433
aositeluoke <458402349@qq.com>
2021-11-02 16:25:43 +0800

    网络请求封装

--- lib/api/api_auth.dart
+++ lib/api/api_auth.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_http/http/http_request.dart';
 
-/*相当于Android Retrofit接口*/
 class ApiAuth {
   static Future getHealthList(
       {@required userId, @required startTime, @required endTime}) async {
